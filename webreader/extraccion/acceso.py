10373948bc8c99fa9e6bccb9a482d4cfba84d96e
Iñigo Mediavilla <imediava2@hotmail.com>
2010-06-13 11:24:45 +0100

    Anadidos nuevos tests a webreader.extraccion.acceso y modificado acceso para tratar el acceso al supermanager en una clase nueva en la que salta excepcion si no consigue autentificar al supermanager o si los campos que se pasan para autentificacion en el POST no son correctos. Estas excepciones ya se han probado en test_acceso.

--- webreader/extraccion/acceso.py
+++ webreader/extraccion/acceso.py
@@ -7,8 +7,14 @@ from webreader.utiles.enums import Enum
 FICHERO_DATOS_USUARIO = 'WebContent/datosUsuarioSM.txt'
 enumPosiciones = Enum("base","alero","pivot")
 
+HTTP_CODE_OK = 200
+HTTP_CODE_ACCEPTED = 202
+HTTP_CODE_FOUND = 302
 
-class AccesoHTTP:	
+
+# Acceso General
+		
+class AccesoHTTP(object):	
 
 	def iniciarConexion(self,url,datos):
 		# Procesador de cookies necesario para mantener la sesion
@@ -18,7 +24,7 @@ class AccesoHTTP:
 		urllib2.install_opener(opener)
 		#Se codifican los datos para mandarlos en el POST
 		authreq_data = urllib.urlencode(datos)
-		urllib2.urlopen(url,authreq_data)
+		return urllib2.urlopen(url,authreq_data)
 
 	def obtenerFicheroPagina(self,url):
 		return self.obtenerRespuesta(url).read()
@@ -30,3 +36,51 @@ class AccesoHTTP:
 		auth_resp = urllib2.urlopen(auth_req)
 		return auth_resp
 
+#Supermanager
+
+#Campos de autentificacion en el supermanager
+CAMPO_AUTENTIF_USUARIO = 'usuario'
+CAMPO_AUTENTIF_CLAVE = 'clave'
+CAMPO_AUTENTIF_CONTROL = 'control'
+
+class AccesoHTTPSupermanager(AccesoHTTP):
+	
+	# Pagina a la que supermanager.acb.com redirecciona cuando la
+	# autenticacion en index.php es correcta
+	PAGINA_REDIRECCION = 'http://supermanager.acb.com/principal.php'
+
+	def iniciarConexion(self,url,datos):
+		campos_necesarios = [CAMPO_AUTENTIF_USUARIO, CAMPO_AUTENTIF_CLAVE,
+				     CAMPO_AUTENTIF_CONTROL]
+		#Comprueba que los campos a pasar para la autentif son correctos
+		#print campos_necesarios
+		lista_pasada = datos.keys()[:]
+		campos_necesarios.sort()
+		lista_pasada.sort()
+		if campos_necesarios != lista_pasada:
+			raise ErrorCamposAutentificacionSupermanager \
+				(campos_necesarios, datos.keys)
+			
+		resp = super(AccesoHTTPSupermanager,self) \
+			.iniciarConexion(url,datos)
+		if resp.geturl() != self.PAGINA_REDIRECCION:
+			raise ErrorAutentificacionSupermanager(url,datos)
+		
+class ErrorAutentificacionSupermanager(Exception):
+	def __init__(self, urlAcceso, datosAcceso):
+		self.url = urlAcceso
+		self.datosUsuario = datosAcceso
+	
+	def __str__(self):
+		return "Fallo al intentar acceder a %s" \
+			" con datos de POST %s." % (self.url,self.datosUsuario)
+
+class ErrorCamposAutentificacionSupermanager(Exception):
+	def __init__(self, camposNecesarios, camposPasados):
+		self.camposNecesarios = camposNecesarios
+		self.camposPasados = camposPasados
+	
+	def __str__(self):
+		return "Fallo son necesarios los siguientes campos %s" \
+			" y se han pasado %s para autentificar." \
+				% (self.camposNecesarios,self.camposPasados)
